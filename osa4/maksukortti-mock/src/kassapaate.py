4ca023bc994c6b42ea17e367c8ae5e43e22f2816
Vilma Vartiainen <vilma.vartiainen@icloud.com>
2025-11-20 12:09:24 +0200

    tehtävä 2

---
+++ osa4/maksukortti-mock/src/kassapaate.py
@@ -0,0 +1,20 @@
+HINTA = 5
+
+
+class Kassapaate:
+    def __init__(self):
+        self.myytyja_lounaita = 0
+
+    def lataa(self, kortti, summa):
+        if summa > 0:
+            kortti.lataa(summa)
+        else:
+            return
+
+    def osta_lounas(self, kortti):
+        if kortti.saldo >= HINTA:
+             kortti.osta(HINTA)
+             self.myytyja_lounaita = self.myytyja_lounaita + 1
+        else:  
+           return
+        
